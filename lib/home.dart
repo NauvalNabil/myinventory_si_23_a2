80a0ad67b713c523c6ffa788f85557cee0d609ac
NauvalNabil <nauvalnabil100@gmail.com>
2025-06-18 16:44:08 +0700

    menambah app bar

--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFF16002F),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 125, 125, 174),
+        title: Row(
+          children: [
+            Icon(Icons.person, color: Colors.white,)
+            
+          ],
+        ),
+      ),
+
+
+      body: Center(
+        child: Container(
+          height: 300,
+          width: 300,
+          decoration: BoxDecoration(
+            color: Colors.deepPurple,
+            borderRadius: BorderRadius.all(Radius.circular(10))
+            
+          ),
+        ),
+      ),
+      
+      
+    );
   }
 }
+
