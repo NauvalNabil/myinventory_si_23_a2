accd007b47fdc00df1d1274ac06fbea3803b0077
NauvalNabil <nauvalnabil100@gmail.com>
2025-06-18 23:02:47 +0700

    script home final

--- lib/home.dart
+++ lib/home.dart
@@ -14,28 +14,79 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: const Color(0xFF16002F),
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 125, 125, 174),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(10)
+          )
+        ),
         title: Row(
           children: [
-            Icon(Icons.person, color: Colors.white,)
-            
+           Container(
+            width: 45,
+            height: 45,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 235, 114, 54),
+              shape: BoxShape.circle
+            ),
+            child: Icon(Icons.person,
+            color: Colors.white,
+            size: 30,
+            ),
+           ),
+           
+           SizedBox(width: 10,),
+           Text ("Selamat datang, ", 
+              style: TextStyle (
+                color: Colors.white,
+                fontSize: 18,
+
+              ),
+            ),
           ],
         ),
       ),
 
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 22, vertical: 16,),
+        child: Row(
+          children: [
+            Container(
+              height: 50,
+              width: 150,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 235, 114, 54),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.add, color: Colors.white,size: 40,),                 
+                ],
+              ),
+            ),
 
-      body: Center(
-        child: Container(
-          height: 300,
-          width: 300,
-          decoration: BoxDecoration(
-            color: Colors.deepPurple,
-            borderRadius: BorderRadius.all(Radius.circular(10))
+            SizedBox(width: 16,),
+            Container(
+              height: 50,
+              width: 150,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 235, 114, 54),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.format_list_bulleted, color: Colors.white,size: 35,),
+                ],
+              ),
+            ),
             
-          ),
+          ],
         ),
       ),
       
       
+
     );
   }
 }
