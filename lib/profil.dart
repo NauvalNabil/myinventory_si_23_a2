5223766e160260d1820c2672da8e67c857f9cf44
SelvianaEka <selvianaekaar@hmail.com>
2025-07-22 09:25:46 +0700

    menambahi ukuran pd sizebox

--- lib/profil.dart
+++ lib/profil.dart
@@ -236,7 +236,7 @@ class _ProfileState extends State<Profile> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        const Icon(Icons.email, color: Color.fromARGB(255, 150, 141, 141)),
+                        const Icon(Icons.email, color: Color.fromARGB(255, 242, 240, 240)),
                         const SizedBox(width: 10),
                         Text(
                           displayEmail ?? 'Tidak tersedia',
