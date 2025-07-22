13f5ef3a2384aa9da45fc8f2569bf0b37f7f4917
SelvianaEka <selvianaekaar@hmail.com>
2025-07-22 09:15:15 +0700

    membenarkan sizebox

--- lib/daftar.dart
+++ lib/daftar.dart
@@ -34,7 +34,7 @@ class _DaftarState extends State<Daftar> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 100),
+                const SizedBox(height: 90),
                 TextField(
                   controller: usernameController,
                   style: const TextStyle(color: Colors.white),
